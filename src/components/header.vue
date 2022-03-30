677900aef6674aea00f1143393c11766c04d497a
dejavu518 <cf_1118sab@163.com>
2022-03-30 10:24:22 +0800

    refactor:header页面重写

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,57 +3,56 @@
     <div class="head">
       <el-dropdown @command="fn">
         <span class="el-dropdown-link">
-          欢迎您,{{info}}
+          欢迎您,{{nickname}}
           <i class="el-icon-arrow-down el-icon--right"></i>
         </span>
         <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item command="a">个人中心</el-dropdown-item>
-          <el-dropdown-item command="b">修改密码</el-dropdown-item>
-          <el-dropdown-item command="c">退出登录</el-dropdown-item>
+          <el-dropdown-item command="my">个人中心</el-dropdown-item>
+          <el-dropdown-item command="change">修改密码</el-dropdown-item>
+          <el-dropdown-item command="exit">退出登录</el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </div>
     <p class="date">
-      今天是你在阿里的第
+      今天是你在my finacial的第
       <span class="tips">{{days}}</span>天
     </p>
     <div style="clear:both"></div>
   </div>
-</template>
 
+</template>
 <script>
-import {get} from "@/utils/http"
-import {removeToken} from "@/utils/auth"
+import { get } from '@/utils/http'
+import { removeToken } from '@/utils/auth'
 export default {
   data() {
     return {
-      time: "",
-      info: sessionStorage.getItem("nickname"),
-    };
+      time: '',
+      nickname: sessionStorage.getItem('nickname')
+    }
   },
   mounted() {
-    get("/in").then((res) => {
-      this.time = res.time;
-    });
+    get('/in').then(res => {
+      this.time = res.time
+    })
   },
   methods: {
     fn(m) {
-      if(m=="c"){
-          removeToken();
-          this.$router.push("/login")
+      if (m === 'exit') {
+        removeToken()
+        this.$router.push('/login')
       }
-    },
+    }
   },
   computed: {
     days() {
-      let target = new Date(this.time);
-      let now = new Date();
-      return Math.floor((now - target) / 1000 / 60 / 60 / 24);
-    },
-  },
-};
+      let target = new Date(this.time)
+      let now = new Date()
+      return Math.floor((now - target) / 1000 / 60 / 60 / 24)
+    }
+  }
+}
 </script>
-
 <style scoped>
 .head {
   float: right;
@@ -66,4 +65,4 @@ export default {
   color: #40a9ff;
   font-size: 24px;
 }
-</style>
+</style>>
